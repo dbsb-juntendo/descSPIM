e4f4f04ec96c3fc696ebdc88c831eb7cc04b68ea
Yuki Nozawa <126061350+YukiNozawa0220@users.noreply.github.com>
2025-03-14 14:07:24 +0900

    Add files via upload

---
+++ descSPIM_Python/DigitalInputPullup_copy_20231223233914__19200b.ino
@@ -0,0 +1,37 @@
+/*
+  Input Pull-up Serial
+
+  This example demonstrates the use of pinMode(INPUT_PULLUP). It reads a digital
+  input on pin 2 and prints the results to the Serial Monitor.
+
+  The circuit:
+  - momentary switch attached from pin 2 to ground
+  - built-in LED on pin 13
+
+  Unlike pinMode(INPUT), there is no pull-down resistor necessary. An internal
+  20K-ohm resistor is pulled to 5V. This configuration causes the input to read
+  HIGH when the switch is open, and LOW when it is closed.
+
+  created 14 Mar 2012
+  by Scott Fitzgerald
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/InputPullupSerial
+*/
+
+void setup() {
+  //start serial connection
+  Serial.begin(19200);
+  //configure pin 2 as an input and enable the internal pull-up resistor
+  pinMode(2, INPUT_PULLUP);
+}
+
+void loop() {
+  //read the pushbutton value into a variable
+  int sensorVal = digitalRead(2);
+  //print out the value of the pushbutton
+  Serial.println(sensorVal);    //<--Must Have command
+  Serial.write(sensorVal);      // With having this line, polling speed is faster !
+
+}
